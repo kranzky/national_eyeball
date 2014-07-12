46e11f631e471a9108d67f0f43291c6bf2561312
Jason Hutchens <jason@agworld.com.au>
2014-07-12 15:55:03 +0800

    add missing postcodes

--- Gemfile
+++ Gemfile
@@ -1,18 +1,13 @@
 source 'https://rubygems.org'
 
-#gem 'rails', '~> 4.1.2'
-#gem 'pg', '~> 0.17.1'
-#gem 'roar-rails', '~> 0.1.6'
-#gem 'sequel-rails', '~> 0.9.3'
-
 group :development do
-# gem 'rake', '~> 10.3.2'
-# gem 'guard', '~> 2.6.1'
   gem 'byebug', '~> 3.1.2'
   gem 'sangaku', '~> 0.5.4'
-# gem 'rubocop', '~> 0.24.0'
-end
-
-group :test do
-# gem 'rspec-rails', '~> 3.0.1'
+  gem 'awesome_print', '~> 1.2.0'
+  gem 'ffi-geos', '~> 1.0.0'
+  gem 'polylines', '~> 0.1.1'
+  gem 'ffi-proj4', '~> 0.1.0'
+  gem 'nokogiri', '~> 1.6.2.1'
+  gem 'execjs', '~> 2.2.1'
+  gem 'progressbar', '~> 0.21.0'
 end
---
+++ scripts/add_postcode_data.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'net/http'
+require 'uri'
+require 'cgi'
+require 'open-uri'
+
+require 'rubygems'
+require 'bundler'
+
+Bundler.require(:development)
+
+CODES = JSON.parse(File.read('codes.json'))
+def add_postcode(filename)
+  data = JSON.parse(File.read(filename))
+  return unless data['state']
+  return unless data['suburb']
+  return if data['postcode']
+  return unless CODES[data['state']]
+  data['postcode'] = CODES[data['state']][data['suburb'].upcase].to_i
+  File.open(filename, "w") { |f| f.write(JSON.pretty_generate(data)) }
+end
+
+bar = ProgressBar.new("doing", 8511)
+Dir.glob("./api/**/statistics.json").each do |filename|
+  bar.inc
+  add_postcode(filename)
+end
