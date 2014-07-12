a138e4910394b44220429b9f029e7e0e2ab4a2a5
Jason Hutchens <jason@agworld.com.au>
2014-07-13 04:31:19 +0800

    more imports

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,43 +25,54 @@ end
 def add_suburb(filename)
   data = JSON.parse(File.read(filename))
 
-  puts filename
   sql = <<-SQL
     INSERT INTO features (type, name, lat, lng, postcode, area, polyline)
     VALUES (
       'Suburb',
-      '#{data["suburb"]}',
+      #{ActiveRecord::Base.connection.quote(data["suburb"])},
       #{data["spatial"]["pole"][0]},
       #{data["spatial"]["pole"][1]},
       #{data["postcode"]},
       #{data["spatial"]["area"]},
-      '#{data["spatial"]["poly"]}'
+      #{ActiveRecord::Base.connection.quote(data["spatial"]["poly"])}
     );
   SQL
   ActiveRecord::Base.connection.execute(sql)
 
   feature_id = ActiveRecord::Base.connection.execute("SELECT MAX(id) FROM features;").to_a[0]["max"].to_i
 
-  add_ato_statistics(feature_id, data['gender'], 'Gender')
-  add_ato_statistics(feature_id, data['age'], 'Age')
-  add_ato_statistics(feature_id, data['school_completed'], 'Level of School Completed')
-  add_ato_statistics(feature_id, data['level_of_education'], 'Further Education')
-  add_ato_statistics(feature_id, data['qualifications'], 'Qualifications')
-  add_ato_statistics(feature_id, data['industry_of_employment'], 'Industry of Employment')
-  add_ato_statistics(feature_id, data['occupation'], 'Occupation')
-  add_ato_statistics(feature_id, data['travel_to_work'], 'Work Commute')
-  add_ato_statistics(feature_id, data['marital_status'], 'Marital Status')
-  add_ato_statistics(feature_id, data['country_of_birth'], 'Country of Birth')
-  add_ato_statistics(feature_id, data['languages_spoken'], 'Languages Spoken')
-  add_ato_statistics(feature_id, data['religion'], 'Religion')
-  add_ato_statistics(feature_id, data['number_of_children'], 'Number of Children')
-  add_ato_statistics(feature_id, data['internet_type'], 'Type of Internet Connection')
+  add_abs_statistics(feature_id, data['gender'], 'Gender')
+  add_abs_statistics(feature_id, data['age'], 'Age')
+  add_abs_statistics(feature_id, data['school_completed'], 'Level of School Completed')
+  add_abs_statistics(feature_id, data['level_of_education'], 'Further Education')
+  add_abs_statistics(feature_id, data['qualifications'], 'Qualifications')
+  add_abs_statistics(feature_id, data['industry_of_employment'], 'Industry of Employment')
+  add_abs_statistics(feature_id, data['occupation'], 'Occupation')
+  add_abs_statistics(feature_id, data['travel_to_work'], 'Work Commute')
+  add_abs_statistics(feature_id, data['marital_status'], 'Marital Status')
+  add_abs_statistics(feature_id, data['country_of_birth'], 'Country of Birth')
+  add_abs_statistics(feature_id, data['languages_spoken'], 'Languages Spoken')
+  add_abs_statistics(feature_id, data['religion'], 'Religion')
+  add_abs_statistics(feature_id, data['number_of_children'], 'Number of Children')
+  add_abs_statistics(feature_id, data['internet_type'], 'Type of Internet Connection')
+
+  return unless data = data['average_tax_return']
+  add_statistic("ATO", "Suburb", feature_id, "Taxpayer Average", nil, 1)
+  add_statistic("ATO", "Suburb", feature_id, "Taxpayer Average", 'Taxable Income', data['taxable_income'])
+  add_statistic("ATO", "Suburb", feature_id, "Taxpayer Average", 'Gross Tax', data['gross_tax'])
+  add_statistic("ATO", "Suburb", feature_id, "Taxpayer Average", 'Medicare Levy', data['medicare_levy'])
+  add_statistic("ATO", "Suburb", feature_id, "Taxpayer Average", 'HELP Debt', data['help_debt'])
+  add_statistic("ATO", "Suburb", feature_id, "Taxpayer Average", 'Gross Interest', data['gross_interest'])
+  add_statistic("ATO", "Suburb", feature_id, "Taxpayer Average", 'Work Expenses', data['work_expenses'])
+  add_statistic("ATO", "Suburb", feature_id, "Taxpayer Average", 'Donations', data['donations'])
+  add_statistic("ATO", "Suburb", feature_id, "Taxpayer Average", 'Child Support', data['child_support'])
+  add_statistic("ATO", "Suburb", feature_id, "Taxpayer Average", 'Gross Rent', data['gross_rent'])
 end
 
-def add_ato_statistics(feature_id, data, heading)
-  add_statistic("ATO", "Suburb", feature_id, heading, nil, data.values.reduce(:+))
+def add_abs_statistics(feature_id, data, heading)
+  add_statistic("ABS", "Suburb", feature_id, heading, nil, data.values.reduce(:+))
   data.each do |name, count|
-    add_statistic("ATO", "Suburb", feature_id, heading, name, count)
+    add_statistic("ABS", "Suburb", feature_id, heading, name, count)
   end
 end
 
@@ -76,8 +87,8 @@ def add_statistic(source, feature, feature_id, heading, name, value)
       VALUES (
         (SELECT id FROM sources WHERE name='#{source}'),
         (SELECT id FROM statistics WHERE name='#{heading}'),
-        '#{name || heading}',
-        '#{feature}'
+        #{ActiveRecord::Base.connection.quote(name || heading)},
+        #{ActiveRecord::Base.connection.quote(feature)}
       );
     SQL
     ActiveRecord::Base.connection.execute(sql)
